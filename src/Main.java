d9eda2b81fc38637b9f17d85d3fa61b68ad0d917
phamthi0605 <116060554+phamthi0605@users.noreply.github.com>
2023-01-30 00:40:55 +0700

    Reverse String

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 
 public class Main {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello");
 
+	public static String reverseString(String str) {
+		String revstr = "";
+		char[] chr = str.toCharArray();
+		for (int i = chr.length - 1; i >= 0; i--) {
+			revstr += chr[i];
+		}
+		return revstr.toString();
+	}
+
+	public static void main(String[] args) {
+		String givenStr = "PHAM THI THI";
+		String revStr;
+		revStr = reverseString(givenStr);
+		System.out.println(revStr);
 	}
 
 }
